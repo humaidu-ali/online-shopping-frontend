87da29f8befa1440e6b2f06df90cd66eab3119ac
humaidu-ali <humaidu.ali@turntabl.io>
2021-03-23 19:36:02 +0000

    navbar,carousel and shopping products interface created

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,19 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
   title = 'online-shop';
+  isNavbarCollapsed=true;
+  // images = [944, 1011, 984].map((n) => `https://picsum.photos/id/${n}/900/500`);
+  
 }
+
+// <div class="carousel-caption">
+// <h2>CLOTHING MADE FOR YOU!</h2>
+// <p>Trending from men and women style collection</p>
+//  <div class="shop-button">
+//    <a id="a" href="">SHOP WOMEN'S</a>
+//    <a id="b" href="">SHOP MEN'S</a>
+ //src="../assets/images/banner_1.png"
